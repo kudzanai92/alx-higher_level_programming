e1e900143173a120c24f6756fc3a2f08d95a23dc
kudzanai92 <keezentpty@gmai.com>
2023-08-30 07:53:11 -0700

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,17 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""Define a class Square."""
+
 
 class Square:
-    """
-    This class represents a simple square.
-    """
-    def __init__(self):
-        """
-        Initialize a new Square instance.
-        """
-        pass
-
-    def dict_(self):
-        """
-        Return a dictionary representation of the Square.
-        """
-        return {}
+    """Represent a square."""
+    pass
